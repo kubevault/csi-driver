169a9332faf11156290b9ef73dc6a153fc66d19d
Tamal Saha <tamal@appscode.com>
2018-12-20 23:17:31 -0800

    Fix flag description
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- pkg/cmds/options/options.go
+++ pkg/cmds/options/options.go
@@ -21,6 +21,6 @@ func NewConfig() *Config {
 
 func (c *Config) AddFlags(fs *pflag.FlagSet) {
 	fs.StringVar(&c.Endpoint, "endpoint", c.Endpoint, "CSI endpoint")
-	fs.StringVar(&c.NodeName, "node", c.NodeName, "Linode Hostname")
+	fs.StringVar(&c.NodeName, "node", c.NodeName, "Hostname")
 
 }
